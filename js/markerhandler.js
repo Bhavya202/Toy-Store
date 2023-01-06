4505eb6e51a112f46e6568f66c14e42d8242c8c2
Bhavya <76608764+Bhavya202@users.noreply.github.com>
2023-01-06 22:30:14 +0530

    Add files via upload

--- js/markerhandler.js
+++ js/markerhandler.js
@@ -1,16 +1,18 @@
 AFRAME.registerComponent("markerhandler", {
   init: async function() {
+    var toys = await this.getToys();
+
     this.el.addEventListener("markerFound", () => {
-      console.log("marker is found");
-      this.handleMarkerFound();
+      var markerId = this.el.id;
+      this.handleMarkerFound(toys, markerId);
     });
 
     this.el.addEventListener("markerLost", () => {
-      console.log("marker is lost");
       this.handleMarkerLost();
     });
   },
-  handleMarkerFound: function() {
+  handleMarkerFound: function(toys, markerId) {
+    // Changing button div visibility
     var buttonDiv = document.getElementById("button-div");
     buttonDiv.style.display = "flex";
 
@@ -21,8 +23,8 @@ AFRAME.registerComponent("markerhandler", {
     orderButtton.addEventListener("click", () => {
       swal({
         icon: "https://i.imgur.com/4NZ6uLY.jpg",
-        title: "Thanks For Order!!",
-        text: "Shipping Within 3-7 Working Days!",
+        title: "Thanks For Order !",
+        text: "  ",
         timer: 2000,
         buttons: false
       });
@@ -32,11 +34,29 @@ AFRAME.registerComponent("markerhandler", {
       swal({
         icon: "warning",
         title: "Order Summary",
-        text: "Work In Progress!!"
+        text: "Work In Progress"
       });
     });
+
+    // Changing Model scale to initial scale
+    var toy = toys.filter(toy => toy.id === markerId)[0];
+
+    var model = document.querySelector(`#model-${toy.id}`);
+    model.setAttribute("position", toy.model_geometry.position);
+    model.setAttribute("rotation", toy.model_geometry.rotation);
+    model.setAttribute("scale", toy.model_geometry.scale);
+  },
+  getToys: async function() {
+    return await firebase
+      .firestore()
+      .collection("toys")
+      .get()
+      .then(snap => {
+        return snap.docs.map(doc => doc.data());
+      });
   },
   handleMarkerLost: function() {
+    // Changing button div visibility
     var buttonDiv = document.getElementById("button-div");
     buttonDiv.style.display = "none";
   }
